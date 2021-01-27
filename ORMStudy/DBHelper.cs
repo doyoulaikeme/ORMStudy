0a8b831eaf82db349145cb5dbd97b38554861c99
强 梁 <673615750@qq.com>
2021-01-27 13:07:48 +0800

    添加基本类。

---
+++ ORMStudy/DBHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ORMStudy
+{
+    public class DBHelper
+    {
+        public T Find<T>(int id) where T : BaseModel
+        {
+
+            Type type = typeof(T);
+
+            var sql = "";
+
+          
+        }
+    }
+}
