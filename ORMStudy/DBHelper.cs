dcc413f5817afc1fd0f7a994ed446ce260c2f83b
强 梁 <673615750@qq.com>
2021-01-27 18:04:07 +0800

    添加通用查询方法。

--- ORMStudy/DBHelper.cs
+++ ORMStudy/DBHelper.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,14 +9,44 @@ namespace ORMStudy
 {
     public class DBHelper
     {
+        private readonly string ConnectionString = "";
+
+        /// <summary>
+        /// 一个方法满足不同表的查询
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="id"></param>
+        /// <returns></returns>
         public T Find<T>(int id) where T : BaseModel
         {
 
             Type type = typeof(T);
+            var columns = string.Join(",", type.GetProperties().Select(p => "[" + p.Name + "]"));
+            var sql = string.Format("select {0} from {1}  where ID='{2}'", columns, type.Name, id);
 
-            var sql = "";
+            using (SqlConnection conn = new SqlConnection(ConnectionString))
+            {
+                SqlCommand command = new SqlCommand(sql, conn);
+                conn.Open();
+                var reader = command.ExecuteReader();
+                if (reader.Read())
+                {
+                    var t = Activator.CreateInstance<T>();
 
-          
+                    foreach (var item in type.GetProperties())
+                    {
+                        item.SetValue(t, reader[item.Name]);
+                    }
+
+                    return t;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+
+
         }
     }
 }
